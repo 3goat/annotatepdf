fccf114271b763dd31c22e378762c6323f8b5c1e
Tristan <nttristan@gmail.com>
2022-06-27 12:20:37 -0700

    .

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ const port = process.env.PORT || 3000
 
 app.set('view engine', 'handlebars');
 app.engine('handlebars', handlebars.engine());
-app.set("views", __dirname + "/views");
+app.set("views", "/views");
+
+// app.set("views", __dirname + "/views");
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
+
+// app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.render('home')
